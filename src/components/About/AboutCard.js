9fa2a123b8eb96d2d5450d42bc6ddf6228d2e890
saumya9889 <msaumya114@gmail.com>
2023-06-14 23:02:07 +0530

    code

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Piyush Agarwal </span>
+            from <span className="purple"> Agra, India.</span>
+            <br /> I am a second year student pursuing MCA from GD Goenka University.
+        
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Reading Novels
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+        
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
